c7af808cb6de94641cb0c93de6d87b1892ac4b60
Arthur N. Guedes <guedes.arthur96@gmail.com>
2023-01-11 09:49:50 -0300

    feat: Exception for when collection is full

---
+++ Assets/Scripts/Exceptions/FullCollectionException.cs
@@ -0,0 +1,7 @@
+﻿public class FullCollectionException : System.Exception
+{
+    public readonly int maxQuantity;
+    public FullCollectionException(string collectionName, int maxQuantity)
+        : base($"Collection {collectionName} is Full [{maxQuantity}]!")
+            => this.maxQuantity = maxQuantity;
+}
