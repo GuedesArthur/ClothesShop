07115d41d16d5fe8f26fb42c2c44260bb2ee4353
Arthur N. Guedes <guedes.arthur96@gmail.com>
2023-01-15 08:48:05 -0300

    feat: Player buying method and Interactions

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,12 +1,41 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
 using static UnityEngine.Input;
 
-public class Controller : MonoBehaviour
+/// <summary>
+/// <see cref="Singleton{TSelf}">Singleton</see> class that read the user's <see cref="UnityEngine.Input">Inputs</see>
+/// and send them to the <see cref="Player"/>
+/// </summary>
+public class Controller : Singleton<Controller>
 {
-    public static IControllable controllable;
+    static IControllable _controllable;
+
+    /// <summary>
+    /// Sets <see cref="IControllable"/> as the Player
+    /// </summary>
+    /// <param name="controlled"></param>
+    public static void SetControllable(IControllable controlled)
+    {
+        if (_controllable == controlled) return;
+
+        if(_controllable != null)
+            OnActionButtonPress.RemoveListener(_controllable.OnActionButtonPress);
+
+        if (controlled != null)
+            OnActionButtonPress.AddListener(controlled.OnActionButtonPress);
+
+        _controllable = controlled;
+    }
+
+    public static UnityEvent OnActionButtonPress = new();
+
     private void Update()
     {
         Vector2 input = new (GetAxis("Horizontal"), GetAxis("Vertical"));
-        controllable.OnInput(new DirectionInput(input));
+        _controllable.OnDirectionalInput(new DirectionInput(input));
+
+        if (GetKeyDown(KeyCode.Space))
+            OnActionButtonPress.Invoke();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,33 +1,64 @@
+using Cysharp.Threading.Tasks;
 using Swizzler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Player : MonoBehaviour, IControllable
 {
     public Wallet wallet;
     public Inventory Inventory;
-    public LinkedList<Clothing> EquippedClothing = new();
+    CardinalAngleSprite baseSprite;
+    public LinkedList<ClothingSprite> EquippedClothing = new();
     public Vector2 velocity;
+    [SerializeField] CardinalAngles _angle;
+    CardinalAngles Angle
+    {
+        get => _angle;
+        set
+        {
+            if (value == _angle) return;
+
+            baseSprite.SimpleAngle = value;
+            foreach (var cs in EquippedClothing)
+                cs.SimpleAngle = value;
+            _angle = value;
+        }
+    }
     public bool IsNude => EquippedClothing.Count == 0;
 
     private void Awake()
     {
-        Controller.controllable = this;
+        baseSprite = GetComponent<CardinalAngleSprite>();
     }
 
-    private void OnDestroy()
-    {
-        Controller.controllable = null;
-    }
+    private void OnEnable() => Controller.SetControllable(this);
+    private void OnDisable() => Controller.SetControllable(null);
 
-    public async void Equip(Clothing clothing)
+    public async void Buy(ClothingData clothing)
     {
-        var overlaps = EquippedClothing.Where(clothing.Overlaps).ToArray();
+        try
+        {
+            wallet -= clothing.price;
+        }
+        catch(Wallet.InsufficientFundsException)
+        {
+            Debug.Log("Cannot afford!");
+            return;
+        }
+
+        Inventory.Add(clothing);
+        await Equip(clothing);
+    }
+    public async UniTask Equip(ClothingData clothing)
+    {
+        var overlaps = EquippedClothing
+            .Where(c => clothing.Overlaps(c)).ToArray();
 
         if(overlaps.Length > 0)
         {
-            var itemNames = string.Join(", ", overlaps.Select(c => c.ToString()));
+            var itemNames = string.Join(", ", overlaps.Select(c => c.Clothing.name));
 
             var wantsToRemove = await Popups.Boolean(
                 $"Your {clothing.name} overlaps with the following items: {itemNames}." +
@@ -42,18 +73,28 @@ public class Player : MonoBehaviour, IControllable
                 Unequip(c);
         }
 
-        EquippedClothing.AddLast(clothing);
-    }
-    public void Unequip(Clothing clothing)
-    {
-        Debug.Assert(!Inventory.IsFull, $"Tried unequipping {clothing.name} with a full Inventory!");
-
-        EquippedClothing.Remove(clothing);
-        Inventory.Add(clothing);
+        var cloth = ClothingSprite.Create(clothing, transform);
+        EquippedClothing.AddLast(cloth);
     }
 
-    void IControllable.OnInput(DirectionInput input)
+    public void Unequip(ClothingData clothing) => Unequip(EquippedClothing.Single(cs => clothing == cs));
+    public void Unequip(ClothingSprite clothingSprite)
     {
-        transform.position += Vector2.Scale(input, velocity * Time.deltaTime).XY0();
+        Debug.Assert(!Inventory.IsFull, $"Tried unequipping {clothingSprite.Clothing.name} with a full Inventory!");
+
+        EquippedClothing.Remove(clothingSprite);
+        Destroy(clothingSprite.gameObject);
+        Inventory.Add(clothingSprite);
     }
+    void IControllable.OnDirectionalInput(DirectionInput input)
+    {
+        if(input.isDead) return;
+
+        transform.position += Vector2.Scale(input, velocity * Time.deltaTime).XYY();
+        Angle = input.cardinalAngle;
+    }
+
+    public void OnActionButtonPress()
+        => InteractableEntity.Nearest(transform.position)
+            .OnInteraction(this);
 }
