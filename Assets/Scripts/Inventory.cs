ac3831be167b14e55ec5683f44ed312b86b3f4aa
Arthur N. Guedes <guedes.arthur96@gmail.com>
2023-01-15 08:47:06 -0300

    feat: Inventory system

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,33 +5,39 @@ using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
+/// <summary>
+/// Contains Items bought by the <see cref="Player"/>
+/// </summary>
 [CreateAssetMenu(menuName = "Inventory")]
-public class Inventory : ScriptableObject, IReadOnlyCollection<Clothing>
+public class Inventory : ScriptableObject, IReadOnlyCollection<ClothingData>
 {
     public const int MaxItems = 40;
 
-    public List<Clothing> ClothingList;
+    public List<ClothingData> ClothingList;
     public bool IsFull => ClothingList.Count >= 40;
     public int Count => ClothingList.Count;
     public int RemainingSlots => MaxItems - ClothingList.Count;
 
-    public void Add(Clothing clothing)
+    public void Add(ClothingData clothing)
     {
         if (IsFull) throw new FullCollectionException(name, MaxItems);
 
         ClothingList.Add(clothing);
     }
 
+    /// <returns><see cref="IEnumerable{T}">IEnumerable</see> containg all <see cref="ClothingData">clothing</see> pieces of the given <see cref="ClothingData.Area">Area</see></returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public IEnumerable<Clothing> FilterByArea(Clothing.Area area)
+    public IEnumerable<ClothingData> FilterByArea(ClothingData.Area area)
         => ClothingList.Where(c => c.equipArea == area);
-    public IEnumerable<Clothing> FilterByName(string pattern)
+
+    /// <returns><see cref="IEnumerable{T}">IEnumerable</see> containg all <see cref="ClothingData">clothing</see> pieces that match the given <see cref="Regex"/> pattern</returns>
+    public IEnumerable<ClothingData> FilterByName(string pattern)
     {
         var regex = new Regex(pattern, RegexOptions.IgnoreCase);
 
         return ClothingList.Where(c => regex.IsMatch(c.name));
     }
 
-    public IEnumerator<Clothing> GetEnumerator() => ClothingList.GetEnumerator();
+    public IEnumerator<ClothingData> GetEnumerator() => ClothingList.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => ClothingList.GetEnumerator();
 }
